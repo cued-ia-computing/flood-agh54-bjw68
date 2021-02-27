0db2265ea85a6a95f319964b11e74dab1e260fda
agh54 <agh54@cam.ac.uk>
2021-02-27 12:05:18 +0000

    2B updates and 2C

--- Task2C.py
+++ Task2C.py
@@ -1,9 +1,9 @@
 from floodsystem.stationdata import build_station_list
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import update_water_levels
 
 def run():
-    """Requirements for Task 2B"""
+    """Requirements for Task 2C"""
 
     # Create a list of stations
     stations = build_station_list()
@@ -12,12 +12,11 @@ def run():
     update_water_levels(stations)
 
     # Finds the stations currently at risk of flooding
-    over_threshold = stations_level_over_threshold(stations, 0.8)
-
+    highest_level = stations_highest_rel_level(stations, 10)
     # Display data
-    for station in over_threshold:
+    for station in highest_level:
         print(station[0].name,station[1])
 
 if __name__ == "__main__":
-    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
     run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -16,3 +16,19 @@ def stations_level_over_threshold(stations, tol):
     #sorts the list in decending order
     flood_sorted = sorted_by_key(flood,1, reverse=True)
     return flood_sorted
+
+def stations_highest_rel_level(stations, N):
+    # creates an empty list
+    flood = []
+
+    # iterates through the list and adds a stationif its data is consistent and its above the tolerance
+    for station in stations:
+        if station.typical_range_consistent() == False or station.relative_water_level() == None:
+            pass
+        else:
+            data = (station, station.relative_water_level())
+            flood.append(data)
+
+    flood_sorted = sorted_by_key(flood,1, reverse=True)
+    return flood_sorted[:N]
+    
